15c6232b68368721e1cb98a3b6ae730aa1a44faa
michto <michto@poczta.onet.pl>
2016-06-05 01:58:55 +0200

    Initial commit

---
+++ lin.py
@@ -0,0 +1,120 @@
+import numpy as np
+import scipy.spatial as ssp
+import numpy.random as nprnd
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+numLow = 1
+numHigh = 100
+numCities = 500
+m = 3
+Dist = np.zeros((numCities, numCities))
+
+
+def generatecities(n):
+    # Generate the coordinates of n random cities
+    xcities = []
+    ycities = []
+    nprnd.seed(18)
+    for x in range(0, n):
+        xcities.append(nprnd.randint(numLow, numHigh))
+        ycities.append(nprnd.randint(numLow, numHigh))
+    return xcities, ycities
+
+
+def plotcities(opttour, xys):
+    # Plots the cities on a square
+    # Input: list of 2 lists with x,y coordinates on each list
+    xy1 = xys[0][:]
+    xy2 = xys[1][:]
+    # Sort according to latest tour optimization
+    xy1 = [xy1[i] for i in opttour]
+    xy2 = [xy2[i] for i in opttour]
+    # Make it a cycle
+    xy1.append(xy1[0])
+    xy2.append(xy2[0])
+    ims.append(plt.plot(xy1, xy2, linestyle='-', marker='o', color='b', markerfacecolor='red'))
+    plt.ylabel('original path')
+
+
+def genDistanceMat(x, y):
+    X = np.array([x, y])
+    distMat = ssp.distance.pdist(X.T)
+    sqdist = ssp.distance.squareform(distMat)
+    return sqdist
+
+
+def calcTourLength(hamPath):
+    tourLength = sum(Dist[hamPath[0:-1], hamPath[1:len(hamPath)]])
+    tourLength += Dist[hamPath[-1], hamPath[0]]
+    return tourLength
+
+
+# Generate cities
+x, y = generatecities(numCities)
+
+Dist = genDistanceMat(x, y)
+# Generate initial tour
+optlist = list(range(0, numCities))
+improvement = 1
+ims = []
+fig1 = plt.figure()
+
+size = 500
+total = 0
+while True:
+    plotcities(optlist, [x, y])
+    count = 0
+    for i in xrange(size - 2):
+        # plotcities(optlist, [x, y])
+        i1 = i + 1
+        for j in xrange(i + 2, size):
+            if j == size - 1:
+                j1 = 0
+            else:
+                j1 = j + 1
+            if i != 0 or j1 != 0:
+                l1 = Dist[optlist[i]][optlist[i1]]
+                l2 = Dist[optlist[j]][optlist[j1]]
+                l3 = Dist[optlist[i]][optlist[j]]
+                l4 = Dist[optlist[i1]][optlist[j1]]
+                if l1 + l2 > l3 + l4:
+                    new_path = optlist[i1:j + 1]
+                    optlist[i1:j + 1] = new_path[::-1]
+                    count += 1
+    total += count
+    if count == 0: break
+
+print('final optlist: ', optlist)
+for i in optlist:
+    print(i, x[i], y[i])
+plotcities(optlist, [x, y])
+im_ani = animation.ArtistAnimation(fig1, ims, interval=20, repeat_delay=300000000000000, blit=True)
+plt.show()
+
+
+
+# def opt2(path):
+#     size = 20
+#     global distance_table
+#     total = 0
+#     while True:
+#         count = 0
+#         for i in xrange(size - 2):
+#             i1 = i + 1
+#             for j in xrange(i + 2, size):
+#                 if j == size - 1:
+#                     j1 = 0
+#                 else:
+#                     j1 = j + 1
+#                 if i != 0 or j1 != 0:
+#                     l1 = Dist[optlist[i]][optlist[i1]]
+#                     l2 = Dist[optlist[j]][optlist[j1]]
+#                     l3 = Dist[optlist[i]][optlist[j]]
+#                     l4 = Dist[optlist[i1]][optlist[j1]]
+#                     if l1 + l2 > l3 + l4:
+#                         new_path = optlist[i1:j+1]
+#                         optlist[i1:j+1] = new_path[::-1]
+#                         count += 1
+#         total += count
+#         if count == 0: break
